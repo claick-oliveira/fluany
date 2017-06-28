818e441708e344684f3a1fdaef923abe16952667
Victor Igor <victor.0w3@hotmail.com>
2017-06-27 22:17:36 -0300

    :fire: changed to data.monad in content

--- src/content/Ask.js
+++ src/content/Ask.js
@@ -3,52 +3,61 @@ import { getIndexThingById } from '../popup/reducers/stateManipulate';
 import { getRandomInt } from '../shared/helpers';
 import { dec, insert, update, assoc, propEq, reject, find, head } from 'ramda';
 import { drawElementAsk } from './ElementAsk.js';
+import Task from 'data.task';
+import Either from 'data.either';
 
-export const loadPacks = async (idPackOfTheAlarm) => {
-  const getElementById = (id, state) => find(propEq('id', id), state);
-  const allPacks = await getInLocal('packState');
-  const indexOfThePack = getIndexThingById(allPacks, idPackOfTheAlarm);
-  const fullPackOfTheAlarm = allPacks[indexOfThePack];
-  const cardsInTraning = fullPackOfTheAlarm.cards;
-  const firstPackInTrain = [{id: idPackOfTheAlarm, cards: cardsInTraning}];
-  let packsInTraning = false;
-
-  /* If first time that is running the play, -> catch, else -> continue */
-  try{
-    packsInTraning = await getInLocal('packsInTraning');
-    console.log('packsInTraning: ', packsInTraning);
-  }catch(e){
-    saveInLocal('packsInTraning', firstPackInTrain);
-    return { packOnAlarm: head(firstPackInTrain), packsInTraning: firstPackInTrain };
-  }
-
-  const elementOfTheAlarm = getElementById(idPackOfTheAlarm, packsInTraning);
-
-  if(elementOfTheAlarm){
-    return { packOnAlarm: elementOfTheAlarm, packsInTraning };
-  }
-
-  //not found element, so insert
-  const packsInTraningWithNewPack = insert(0, head(firstPackInTrain), packsInTraning);
-  saveInLocal('packsInTraning', packsInTraningWithNewPack);
-  return { packOnAlarm: head(firstPackInTrain), packsInTraning };
-};
-
+export const getElementById = (id, state) => find(propEq('id', id), state);
 export const getRandomCard = (cards) => {
 	const indexCardBeingUsed = getRandomInt(0, dec(cards.length));
 	return cards[indexCardBeingUsed];
 };
 
-export const ask = async (idPackInTraning, alarmName, periodInMinutes) => {
-  const packs = await loadPacks(idPackInTraning);
-  if(packs.packOnAlarm.cards.length > 0){
-    const card = getRandomCard(packs.packOnAlarm.cards);
-    const doSuccess = () => {
-      const newCards = reject(propEq('id', card.id), packs.packOnAlarm.cards);
-      const index = getIndexThingById(packs.packsInTraning, idPackInTraning);
-      const packsWithoutCardThatHit = update(index, assoc('cards', newCards, packs.packsInTraning[index]), packs.packsInTraning);
-      saveInLocal('packsInTraning', packsWithoutCardThatHit);
-    };
-    drawElementAsk(card.front, card.back, doSuccess, alarmName, periodInMinutes);
-  }
-};
+const load = (name) => new Task((reject, resolve) => {
+	getInLocal(name)
+		.then(data => resolve(data))
+		.catch(e => reject(`Non-existing ${name} in local`));
+});
+
+const makeNice = either =>
+			either.isRight ?
+      Task.of(either.value) :
+      Task.rejected(either.value);
+
+export const ask = (idPackInTraning, alarmName, periodInMinutes) => {
+	const getIndexOfThePack = (packs) => {
+		return getIndexThingById(packs, idPackInTraning) === -1
+			? Either.Left(`Non-existing pack with id:${idPackInTraning}`)
+			: Either.Right(getIndexThingById(packs, idPackInTraning));
+	};
+
+	const mappedTraning = future => future.map(packs => {
+		const elementOfTheAlarm = getElementById(idPackInTraning, packs);
+		if(elementOfTheAlarm){
+			return Either.Right({ packOnAlarm: elementOfTheAlarm, packsInTraning: packs});
+		}
+	});
+
+	const loadTraning = mappedTraning(load('packsInTraning'));
+
+	const mappedPack = future => future.map(packs => {
+		const eitherIndex = getIndexOfThePack(packs);
+		if(eitherIndex.isRight){
+			const firstPackInTrain = [{id: idPackInTraning, cards: packs[eitherIndex.value].cards}];
+			const traning = loadTraning.chain(makeNice);
+			traning.fork(
+				() => {
+					return Either.Left('>: ');
+				},
+				(traning) => {
+					return Either.Right(traning);
+				});
+		}
+		return eitherIndex;
+	});
+
+	const a = mappedPack(load('packState'));
+	const concatenad = a.chain(makeNice);
+	concatenad.fork(console.error, (packs) => {
+		console.log('index: ', packs);
+	});
+																	 };
